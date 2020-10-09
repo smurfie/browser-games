6f57ebdf75a3c5477269737f141e8548d36937d9
Joan Fluvià Poyatos <joanfluvia@gmail.com>
2020-10-09 15:08:34 +0200

    feat: add google analytics

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const PORT = process.env.PORT || 3000;
 const GAMES = ['snaek', 'maze'];
+const ENV = process.env.NODE_ENV || 'localhost'; // 'production' in production
 
 let express = require('express'),
-  app = express();
+  app = express()
+  isProduction = ENV === 'production';
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -11,28 +13,24 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-  res.render('home');
-});
-
-app.get('/home', function(req, res) {
-  res.render('home');
+  res.render('home', {isProduction: isProduction});
 });
 
 app.get('/game/:game?', function(req, res) {
   var game = req.params.game;
   if (GAMES.indexOf(game)>=0) {
-    res.render(game);
+    res.render(game, {isProduction: isProduction});
   } else {
     res.send('Game: ' + game + " doesn't exists");
   }
 });
 
 app.get('/game/snaek', function(req, res) {
-  res.render('snaek');
+  res.render('snaek', {isProduction: isProduction});
 });
 
 app.get('/game/maze', function(req, res) {
-  res.render('maze');
+  res.render('maze', {isProduction: isProduction});
 });
 
 app.listen(PORT, () => {
